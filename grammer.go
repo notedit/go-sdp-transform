6c3d3e740057796b27a24fb77b18b45defd59cb9
notedit <notedit@gmail.com>
2018-07-08 17:00:31 +0800

    add more rules

--- grammer.go
+++ grammer.go
@@ -2,6 +2,8 @@ package sdptransform
 
 import (
 	"regexp"
+
+	json "github.com/bitly/go-simplejson"
 )
 
 type Rule struct {
@@ -12,6 +14,23 @@ type Rule struct {
 	Types      []rune
 	Format     string
 	FormatFunc interface{}
+}
+
+func hasValue(obj *json.Json, key string) bool {
+	if _, ok := obj.CheckGet(key); !ok {
+		return false
+	}
+	value := obj.Get(key)
+	if str, err := value.String(); err != nil {
+		if len(str) == 0 {
+			return false
+		}
+		return true
+	} else if _, err := value.Int(); err != nil {
+		return true
+	} else {
+		return false
+	}
 }
 
 var RulesMap map[byte][]*Rule = map[byte][]*Rule{
@@ -288,5 +307,233 @@ var RulesMap map[byte][]*Rule = map[byte][]*Rule{
 			Format: "%s",
 		},
 		// a=ice-ufrag:F7gI
+		&Rule{
+			Name:   "iceUfrag",
+			Push:   "",
+			Reg:    regexp.MustCompile("^ice-ufrag:(\\S*)"),
+			Names:  []string{},
+			Types:  []rune{'s'},
+			Format: "ice-ufrag:%s",
+		},
+		// a=ice-pwd:x9cml/YzichV2+XlhiMu8g
+		&Rule{
+			Name:   "icePwd",
+			Push:   "",
+			Reg:    regexp.MustCompile("^ice-pwd:(\\S*)"),
+			Names:  []string{},
+			Types:  []rune{'s'},
+			Format: "ice-pwd:%s",
+		},
+		// a=fingerprint:SHA-1 00:11:22:33:44:55:66:77:88:99:AA:BB:CC:DD:EE:FF:00:11:22:33
+		&Rule{
+			Name:   "fingerprint",
+			Push:   "",
+			Reg:    regexp.MustCompile("^fingerprint:(\\S*) (\\S*)"),
+			Names:  []string{"type", "hash"},
+			Types:  []rune{'s', 's'},
+			Format: "fingerprint:%s %s",
+		},
+		// a=candidate:0 1 UDP 2113667327 203.0.113.1 54400 typ host
+		// a=candidate:1162875081 1 udp 2113937151 192.168.34.75 60017 typ host generation 0 network-id 3 network-cost 10
+		// a=candidate:3289912957 2 udp 1845501695 193.84.77.194 60017 typ srflx raddr 192.168.34.75 rport 60017 generation 0 network-id 3 network-cost 10
+		// a=candidate:229815620 1 tcp 1518280447 192.168.150.19 60017 typ host tcptype active generation 0 network-id 3 network-cost 10
+		// a=candidate:3289912957 2 tcp 1845501695 193.84.77.194 60017 typ srflx raddr 192.168.34.75 rport 60017 tcptype passive generation 0 network-id 3 network-cost 10
+		&Rule{
+			Name:       "",
+			Push:       "candidates",
+			Reg:        regexp.MustCompile("^candidate:(\\S*) (\\d*) (\\S*) (\\d*) (\\S*) (\\d*) typ (\\S*)(?: raddr (\\S*) rport (\\d*))?(?: tcptype (\\S*))?(?: generation (\\d*))?(?: network-id (\\d*))?(?: network-cost (\\d*))?"),
+			Names:      []string{"foundation", "component", "transport", "priority", "ip", "port", "type", "raddr", "rport", "tcptype", "generation", "network-id", "network-cost"},
+			Types:      []rune{'s', 'd', 's', 'd', 's', 'd', 's', 's', 'd', 's', 'd', 'd', 'd', 'd'},
+			Format:     "",
+			FormatFunc: nil,
+		},
+		// a=end-of-candidates
+		&Rule{
+			Name:   "endOfCandidates",
+			Push:   "",
+			Reg:    regexp.MustCompile("^(end-of-candidates)"),
+			Names:  []string{},
+			Types:  []rune{'s'},
+			Format: "%s",
+		},
+		// a=remote-candidates:1 203.0.113.1 54400 2 203.0.113.1 54401
+		&Rule{
+			Name:   "remoteCandidates",
+			Push:   "",
+			Reg:    regexp.MustCompile("^remote-candidates:(.*)"),
+			Names:  []string{},
+			Types:  []rune{'s'},
+			Format: "remote-candidates:%s",
+		},
+		// a=ice-options:google-ice
+		&Rule{
+			Name:   "iceOptions",
+			Push:   "",
+			Reg:    regexp.MustCompile("^ice-options:(\\S*)"),
+			Names:  []string{},
+			Types:  []rune{'s'},
+			Format: "ice-options:%s",
+		},
+		// a=ssrc:2566107569 cname:t9YU8M1UxTF8Y1A1
+		&Rule{
+			Name:       "",
+			Push:       "ssrcs",
+			Reg:        regexp.MustCompile("^ssrc:(\\d*) ([\\w_-]*)(?::(.*))?"),
+			Names:      []string{"id", "attribute", "value"},
+			Types:      []rune{'d', 's', 's'},
+			Format:     "",
+			FormatFunc: nil,
+		},
+		// a=ssrc-group:FEC 1 2
+		// a=ssrc-group:FEC-FR 3004364195 1080772241
+		&Rule{
+			Name:   "",
+			Push:   "ssrcGroups",
+			Reg:    regexp.MustCompile("^ssrc-group:([\x21\x23\x24\x25\x26\x27\x2A\x2B\x2D\x2E\\w]*) (.*)"),
+			Names:  []string{"semantics", "ssrcs"},
+			Types:  []rune{'s', 's'},
+			Format: "ssrc-group:%s %s",
+		},
+		// a=msid-semantic: WMS Jvlam5X3SX1OP6pn20zWogvaKJz5Hjf9OnlV
+		&Rule{
+			Name:   "msidSemantic",
+			Push:   "",
+			Reg:    regexp.MustCompile("^msid-semantic:\\s?(\\w*) (\\S*)"),
+			Names:  []string{"semantic", "token"},
+			Types:  []rune{'s', 's'},
+			Format: "msid-semantic: %s %s",
+		},
+		// a=group:BUNDLE audio video
+		&Rule{
+			Name:   "",
+			Push:   "groups",
+			Reg:    regexp.MustCompile("^group:(\\w*) (.*)"),
+			Names:  []string{"type", "mids"},
+			Types:  []rune{'s', 's'},
+			Format: "group:%s %s",
+		},
+		// a=rtcp-mux
+		&Rule{
+			Name:   "rtcpMux",
+			Push:   "",
+			Reg:    regexp.MustCompile("^(rtcp-mux)"),
+			Names:  []string{},
+			Types:  []rune{'s'},
+			Format: "%s",
+		},
+		// a=rtcp-rsize
+		&Rule{
+			Name:   "rtcpRsize",
+			Push:   "",
+			Reg:    regexp.MustCompile("^(rtcp-rsize)"),
+			Names:  []string{},
+			Types:  []rune{'s'},
+			Format: "%s",
+		},
+		// a=sctpmap:5000 webrtc-datachannel 1024
+		&Rule{
+			Name:       "sctpmap",
+			Push:       "",
+			Reg:        regexp.MustCompile("^sctpmap:(\\d+) (\\S*)(?: (\\d*))?"),
+			Names:      []string{"sctpmapNumber", "app", "maxMessageSize"},
+			Types:      []rune{'d', 's', 'd'},
+			Format:     "",
+			FormatFunc: nil,
+		},
+		// a=x-google-flag:conference
+		&Rule{
+			Name:   "xGoogleFlag",
+			Push:   "",
+			Reg:    regexp.MustCompile("x-google-flag:([^\\s]*)"),
+			Names:  []string{},
+			Types:  []rune{'s'},
+			Format: "x-google-flag:%s",
+		},
+		// a=rid:1 send max-width=1280;max-height=720;max-fps=30;depend=0
+		&Rule{
+			Name:       "",
+			Push:       "rids",
+			Reg:        regexp.MustCompile("^rid:([\\d\\w]+) (\\w+)(?: ([\\S| ]*))?"),
+			Names:      []string{"id", "direction", "params"},
+			Types:      []rune{'s', 's', 's'},
+			Format:     "",
+			FormatFunc: nil,
+		},
+		// a=imageattr:97 send [x=800,y=640,sar=1.1,q=0.6] [x=480,y=320] recv [x=330,y=250]
+		// a=imageattr:* send [x=800,y=640] recv *
+		// a=imageattr:100 recv [x=320,y=240]
+		&Rule{
+			Name: "",
+			Push: "imageattrs",
+			Reg: regexp.MustCompile(
+				"^imageattr:(\\d+|\\*)" +
+					"[\\s\\t]+(send|recv)[\\s\\t]+(\\*|\\[\\S+\\](?:[\\s\\t]+\\[\\S+\\])*)" +
+					"(?:[\\s\\t]+(recv|send)[\\s\\t]+(\\*|\\[\\S+\\](?:[\\s\\t]+\\[\\S+\\])*))?"),
+			Names: []string{
+				"pt",
+				"dir1",
+				"attrs1",
+				"dir2",
+				"attrs2",
+			},
+			Types:      []rune{'s', 's', 's', 's', 's'},
+			Format:     "",
+			FormatFunc: nil,
+		},
+		// a=simulcast:send 1,2,3;~4,~5 recv 6;~7,~8
+		// a=simulcast:recv 1;4,5 send 6;7
+		&Rule{
+			Name: "simulcast",
+			Push: "",
+			Reg: regexp.MustCompile(
+				"^simulcast:" +
+					"(send|recv) ([a-zA-Z0-9\\-_~;,]+)" +
+					"(?:\\s?(send|recv) ([a-zA-Z0-9\\-_~;,]+))?" +
+					"$"),
+			Names:      []string{"dir1", "list1", "dir2", "list2"},
+			Types:      []rune{'s', 's', 's', 's'},
+			Format:     "",
+			FormatFunc: nil,
+		},
+		// Old simulcast draft 03 (implemented by Firefox).
+		// https://tools.ietf.org/html/draft-ietf-mmusic-sdp-simulcast-03
+		// a=simulcast: recv pt=97;98 send pt=97
+		// a=simulcast: send rid=5;6;7 paused=6,7
+		&Rule{
+			Name:   "simulcast_03",
+			Push:   "",
+			Reg:    regexp.MustCompile("^simulcast:[\\s\\t]+([\\S+\\s\t]+)$"),
+			Names:  []string{"value"},
+			Types:  []rune{'s'},
+			Format: "simulcast: %s",
+		},
+		// a=framerate:25
+		// a=framerate:29.97
+		&Rule{
+			Name:   "framerate",
+			Push:   "",
+			Reg:    regexp.MustCompile("^framerate:(\\d+(?:$|\\.\\d+))"),
+			Names:  []string{},
+			Types:  []rune{'f'},
+			Format: "framerate:%s",
+		},
+		// a=source-filter: incl IN IP4 239.5.2.31 10.1.15.5
+		&Rule{
+			Name:   "sourceFilter",
+			Push:   "",
+			Reg:    regexp.MustCompile("^source-filter:[\\s\\t]+(excl|incl) (\\S*) (IP4|IP6|\\*) (\\S*) (.*)"),
+			Names:  []string{"filterMode", "netType", "addressTypes", "destAddress", "srcList"},
+			Types:  []rune{'s', 's', 's', 's', 's'},
+			Format: "source-filter: %s %s %s %s %s",
+		},
+		// Any a= that we don't understand is kepts verbatim on media.invalid.
+		&Rule{
+			Name:   "",
+			Push:   "invalid",
+			Reg:    regexp.MustCompile("(.*)"),
+			Names:  []string{"value"},
+			Types:  []rune{'s'},
+			Format: "%s",
+		},
 	},
 }
